73784f77ba79f9441d70756ad03f78eef89d901d
Craig Wattrus <cwattrus@gmail.com>
2013-09-28 13:37:03 +0200

    Trying to pull the meetup info via jsonp and jquery ajax

--- application.js
+++ application.js
@@ -1,3 +1,10 @@
-$.getJSON( "http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&group_urlname=http%3A%2F%2Fwww.meetup.com%2FCodedInBraam%2F&desc=false&offset=0&format=json&page=20&fields=&sig_id=42203632&sig=93f58ef2749515567ebf35f30b87cb245730e969", function( data ) {
-  console.log(data);
+
+$(document).ready(function() {
+    var url =  "http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&group_urlname=http%3A%2F%2Fwww.meetup.com%2FCodedInBraam%2F&desc=false&offset=0&format=json&page=20&fields=&sig_id=42203632&sig=93f58ef2749515567ebf35f30b87cb245730e969";
+    $.getJSON(url + "?callback=?", null, function(data) {
+        for(i in data) {
+            dat = data[i];
+            $("#data-list").append(dat + "<hr />");
+        }
+    });
 });
